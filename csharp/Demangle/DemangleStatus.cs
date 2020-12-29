2628306dc056f0a0ba2c9551575ce9f09191c350
Yretenai <yretenai@gmail.com>
2020-12-29 09:45:19 +0000

    add missing notices

--- csharp/Demangle/DemangleStatus.cs
+++ csharp/Demangle/DemangleStatus.cs
@@ -1,4 +1,13 @@
-﻿namespace Demangle
+﻿// Copyright 2020 Yretenai
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// https://github.com/yretenai/demangle/blob/develop/LICENSE
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+namespace Demangle
 {
     public enum DemangleStatus
     {
